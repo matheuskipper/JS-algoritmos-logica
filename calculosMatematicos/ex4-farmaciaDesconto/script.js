37c9ce7f503b1ce3ee01e4c61c8ab0ec890dd4bb
Matheus Kipper <matheuskipper9@gmail.com>
2025-06-17 16:26:51 -0300

    Atualização exercícios

--- calculosMatematicos/ex4-farmaciaDesconto/script.js
+++ calculosMatematicos/ex4-farmaciaDesconto/script.js
@@ -6,4 +6,13 @@ frm.addEventListener('submit', (e) => {
 
     const produto = frm.iNomeProduto.value;
     const preco = Number(frm.inPreco.value);
-})
+
+    const desconto = preco * 0.10;
+    const valorComDesconto = preco - desconto;
+
+    if (produto == '' || preco == ''){
+        result.innerText = 'Preencha os dois campos para prosseguir'
+    } else {
+        result.innerText = `Promoção: ${produto} - De R$ ${preco} por R$ ${valorComDesconto}`
+    }
+});
