a67578d88567589b1caf61fedd262bb4e3bcb6a6
Matheus Kipper <matheuskipper9@gmail.com>
2025-06-24 17:59:06 -0300

    adicionando exercicios

---
+++ integracaoHtml/ex7-notaAluno/script.js
@@ -0,0 +1,19 @@
+const frm = document.getElementById('formulario');
+const result1 = document.getElementById('resultado1');
+const result2 = document.getElementById('resultado2');
+
+frm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nome = frm.inNome.value
+
+    const nota1 = Number(frm.inNota1.value)
+    const nota2 = Number(frm.inNota2.value)
+    const nota3 = Number(frm.inNota3.value)
+
+    const somaNotas = nota1 + nota2 + nota3
+    const mediaNotas = somaNotas / 3
+    
+    result1.innerText = `Aluno: ${nome}`;
+    result2.innerText = `A média do aluno foi: ${mediaNotas.toFixed(1)}`
+})
