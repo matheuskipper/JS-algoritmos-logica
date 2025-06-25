6b49d4956627268a45d9f7c89e3a69614bd0bbfe
Matheus Kipper <matheuskipper9@gmail.com>
2025-06-24 23:21:57 -0300

    update exercicio

--- integracaoHtml/ex7-notaAluno/script.js
+++ integracaoHtml/ex7-notaAluno/script.js
@@ -1,19 +1,28 @@
-const frm = document.getElementById('formulario');
-const result1 = document.getElementById('resultado1');
-const result2 = document.getElementById('resultado2');
+const frm = document.getElementById("formulario");
+const result1 = document.getElementById("resultado1");
+const result2 = document.getElementById("resultado2");
 
-frm.addEventListener('submit', (e) => {
-    e.preventDefault();
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const nome = frm.inNome.value
+  const nome = frm.inNome.value;
 
-    const nota1 = Number(frm.inNota1.value)
-    const nota2 = Number(frm.inNota2.value)
-    const nota3 = Number(frm.inNota3.value)
+  const nota1 = Number(frm.inNota1.value);
+  const nota2 = Number(frm.inNota2.value);
+  const nota3 = Number(frm.inNota3.value);
 
-    const somaNotas = nota1 + nota2 + nota3
-    const mediaNotas = somaNotas / 3
-    
+  const somaNotas = nota1 + nota2 + nota3;
+  const mediaNotas = somaNotas / 3;
+
+  if (mediaNotas >= 6) {
     result1.innerText = `Aluno: ${nome}`;
-    result2.innerText = `A média do aluno foi: ${mediaNotas.toFixed(1)}`
-})
+    result2.innerText = `A média do aluno foi: ${mediaNotas.toFixed(
+      1
+    )} - Parabéns! Você está aprovado!`;
+  } else {
+    result1.innerText = `Aluno: ${nome}`;
+    result2.innerText = `A média do aluno foi: ${mediaNotas.toFixed(
+      1
+    )} - Infelizmente, você está reprovado...`;
+  }
+});
