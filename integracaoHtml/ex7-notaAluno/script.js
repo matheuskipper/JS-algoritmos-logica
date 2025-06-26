1ea7a9149439ce8f5d3554f7bfa22f97a6842e28
Matheus Kipper <matheuskipper9@gmail.com>
2025-06-25 23:39:36 -0300

    update

--- integracaoHtml/ex7-notaAluno/script.js
+++ integracaoHtml/ex7-notaAluno/script.js
@@ -19,12 +19,18 @@ frm.addEventListener("submit", (e) => {
     result2.innerText = `A média do aluno foi: ${mediaNotas.toFixed(
       1
     )} - Parabéns! Você está aprovado!`;
-    result2.style.color = 'blue'
+    result2.style.color = "blue";
+  } else if (mediaNotas >= 4) {
+    result1.innerText = `Aluno: ${nome}`;
+    result2.innerText = `A  média do aluno foi: ${mediaNotas.toFixed(
+      1
+    )} - Atenção, o aluno está em recuperação!`;
+    result2.style.color = "yellow";
   } else {
     result1.innerText = `Aluno: ${nome}`;
     result2.innerText = `A média do aluno foi: ${mediaNotas.toFixed(
       1
     )} - Infelizmente, você está reprovado...`;
-    result2.style.color = 'red'
+    result2.style.color = "red";
   }
 });
