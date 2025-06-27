73ba75aa3e098b21ee3f862eef59df9b39a64eef
Matheus Kipper <matheuskipper9@gmail.com>
2025-06-26 23:52:21 -0300

    primeiro commit na branch nova

--- calculoPesoIdeal/script.js
+++ calculoPesoIdeal/script.js
@@ -1,24 +1,36 @@
-const frm = document.getElementById('formulario');
-const result = document.getElementById('resultado');
+const frm = document.getElementById("formulario");
+const result = document.getElementById("resultado");
 
-frm.addEventListener('submit', (e) => {
-    e.preventDefault();
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const nome = document.getElementById('inNome').value;
-    let altura = parseFloat(document.getElementById('inAltura').value);
-    const sexoselecionado = document.querySelector('input[name="sexo"]:checked');
-    const sexo = sexoselecionado.value;
+  const nome = document.getElementById("inNome").value;
+  let altura = parseFloat(document.getElementById("inAltura").value);
+  const sexoselecionado = document.querySelector('input[name="sexo"]:checked');
+  const sexo = sexoselecionado.value;
 
-    if (altura > 3) {
-        altura = altura / 100
+  if (altura > 3) {
+    altura = altura / 100;
+  }
+
+  // Pode ser feito com essa estrutura condicional:
+  function calculoPeso() {
+    if (sexo == "masculino") {
+      return (
+        nome + ", Seu peso ideal é: " + (22 * (altura * altura)).toFixed(2)
+      );
+    } else {
+      return (
+        nome + ", Seu peso ideal é: " + (21 * (altura * altura)).toFixed(2)
+      );
     }
-    
-    function calculoPeso () {
-        if(sexo == 'masculino'){
-            return nome + ", Seu peso ideal é: " + (22 * (altura * altura)).toFixed(2);
-        } else {
-            return nome + ", Seu peso ideal é: " + (21 * (altura * altura)).toFixed(2);
-        }
-    }
-    result.innerText = calculoPeso();
+  }
+
+  // OU essa:
+  function calculoPeso() {
+    return sexo === "masculino"
+      ? nome + ", Seu peso ideal é: " + (22 * (altura * altura)).toFixed(2)
+      : nome + ", Seu peso ideal é: " + (21 * (altura * altura)).toFixed(2);
+  }
+  result.innerText = calculoPeso();
 });
